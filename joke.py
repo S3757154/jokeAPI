3f6baba3a45f002a0069665926e7ae656c4b9277
S3757154 <S3757154@students.fscj.edu>
2023-03-11 10:29:25 -0800

    joke.py

---
+++ joke.py
@@ -0,0 +1,14 @@
+#Modified
+import requests
+
+url = "https://icanhazdadjoke.com"
+
+payload={}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
